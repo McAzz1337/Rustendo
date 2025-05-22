9cdfe3a53873ac43f8c102d1c19637dadded6612
MarcF <marcf91.mf@gmail.com>
2025-05-23 00:11:26 +0200

    Removed uneccessary macros

--- src/main.rs
+++ src/main.rs
@@ -8,28 +8,6 @@ pub mod utils;
 use consoles::cartridge::{self};
 use consoles::console::{self, Console};
 
-macro_rules! _assert_eq {
-    ($a: expr, $b: expr) => {
-        if ($a != $b) {
-            eprintln!(
-                "Unexpected value [{}]\n\texpected: {}\tactual: {}",
-                stringify!($a),
-                $b,
-                $a
-            );
-        }
-    };
-}
-
-macro_rules! _assert {
-    ($a: expr, $b: expr, $c: expr) => {
-        if (!$a) {
-            eprintln!("expected: {}\tactual: {}", $b, $c);
-        }
-        assert!($a);
-    };
-}
-
 fn main() {
     let file = String::from("roms/Pokemon-Silver.gbc");
 
