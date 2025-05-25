bb24593a6dc52fe1e691462463496fbbd0b74c81
MarcF <marcf91.mf@gmail.com>
2025-05-25 13:16:59 +0200

    Added log macro for logging the call to a function

--- src/macros.rs
+++ src/macros.rs
@@ -1,7 +1,25 @@
+use crate::function_name;
+
 #[macro_export]
 macro_rules! trace {
     ($e: expr) => {{
         println!("{} {}: {}", line!(), file!(), stringify!($e));
         $e
+    }};
+}
+
+#[macro_export]
+macro_rules! log {
+    () => {{
+        println!("{} {} => : {}", line!(), file!(), function_name!())
+    }};
+    ($e: expr) => {{
+        println!(
+            "{} {} => : {} -> {}",
+            line!(),
+            file!(),
+            function_name!(),
+            format!($e)
+        )
     }};
 }
